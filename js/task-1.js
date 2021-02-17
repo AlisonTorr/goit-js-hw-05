75144ae7ad7e85c7360132c3780e87c84f43709d
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-02-17 19:53:18 +0300

    Add files via upload

---
+++ js/task-1.js
@@ -0,0 +1,24 @@
+const Account = function (login, password) {
+  this.login = login;
+  this.password = password;
+};
+
+Account.prototype.getInfo = function (login, password) {
+  console.log(`login: ${login} , password: ${password}`);
+};
+
+console.log(Account.prototype.getInfo); // function
+
+const mango = new Account({
+  login: "Mangozedog",
+  email: "mango@dog.woof",
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+  login: "Poly",
+  email: "poly@mail.com",
+});
+
+poly.getInfo();
