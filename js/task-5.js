09acb9fb21ba0e81f015603b54b3bd5b14425a57
AlisonTorr <70812870+AlisonTorr@users.noreply.github.com>
2021-02-17 21:15:09 +0300

    Add files via upload

--- js/task-5.js
+++ js/task-5.js
@@ -28,4 +28,43 @@ class Car {
   turnOff() {
     this.isOn = false;
   }
+
+  accelerate(value) {
+    this.speed += value;
+    const totalSpeed = this.speed < this.maxSpeed ? this.speed : this.maxSpeed;
+    return totalSpeed;
+  }
+
+  decelerate(value) {
+    const decSpeed = this.speed - value;
+    const totalSpeed = decSpeed > 0 ? decSpeed : this.speed;
+    return totalSpeed;
+  }
+
+  drive(hours) {
+    if (this.isOn) {
+      this.distance = hours * this.speed;
+    }
+    return this.distance;
+  }
 }
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
